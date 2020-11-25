06f40a937d429c8cb17c51071b52cd922800e3e9
Fernando Arellano <farellano0@ucol.mx>
2020-11-25 13:45:49 -0600

    Implemented esPar()

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 export default class App {
  
+    esPar(numero){
+        if (numero % 2 == 0){
+            return true;
+        } else {return false}
+    }
+
+
 }
 
 let app = new App();
+
+console.log(app.esPar(4));
+console.log(app.esPar(5));
