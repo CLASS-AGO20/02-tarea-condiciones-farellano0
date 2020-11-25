ab5a6029723f97ff05ce504371f8099e72a3e524
Fernando Arellano <farellano0@ucol.mx>
2020-11-25 14:11:14 -0600

    implemented esMultiplo()

--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
 export default class App {
  
     esPar(numero){
-        if (numero % 2 == 0){
+        if (numero == 0){
+            return false;
+        } else if (numero % 2 == 0){
             return true;
-        } else {return false}
+        } else {
+            return false;
+        }
     }
+
+    esMultiplo(numero1, numero2){
+        if (numero1 % numero2 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 
 }
 
 let app = new App();
 
+console.log("Probando esPar()");
 console.log(app.esPar(4));
 console.log(app.esPar(5));
+console.log(app.esPar(0));
+
+console.log("Probando esMultiplo()");
+console.log(app.esMultiplo(10, 5));
+console.log(app.esMultiplo(9, 4));
