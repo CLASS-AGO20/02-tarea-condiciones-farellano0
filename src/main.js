8a81288bdaba279763d52321a8dd6781cb6ade92
Fernando Arellano <farellano0@ucol.mx>
2020-11-25 16:16:48 -0600

    Implemented calcular()

--- src/main.js
+++ src/main.js
@@ -32,7 +32,6 @@ export default class App {
     }
 
     obtenerCalificacion(calificacionNumerica){
-        
         switch(calificacionNumerica){
             case 0:
                 return "C";
@@ -62,6 +61,41 @@ export default class App {
 
     }
 
+    obtenerCercano100(numero1, numero2, numero3){
+        let x = Math.abs(numero1 - 100);
+        let y = Math.abs(numero2 - 100);
+        let z = Math.abs(numero3 - 100);
+
+        if(x < y && x < z){
+            return numero1;
+        } else if (y < x && y < z){
+            return numero2;
+        } else if (z < x && z < y){
+            return numero3;
+        } else {
+            return numero1;
+        }
+
+    }
+
+    calcular(numero1, numero2, operador){
+        switch(operador){
+            case '+':
+                return numero1 + numero2;
+            case '-':
+                return numero1 - numero2;
+            case '*':
+                return numero1 * numero2;
+            case '/':
+                return numero1 / numero2;
+            case '%':
+                return numero1 % numero2;
+            default:
+                return -1;
+        }
+    }
+
+
 
 
 
@@ -84,7 +118,7 @@ console.log(app.calcularSalario(2, 8));
 console.log(app.calcularSalario(3, 8));
 console.log(app.calcularSalario(4, 8));
 
-console.log("Probando obtenerCalificacion()")
+console.log("Probando obtenerCalificacion()");
 console.log(app.obtenerCalificacion(0));
 console.log(app.obtenerCalificacion(1));
 console.log(app.obtenerCalificacion(2));
@@ -97,3 +131,15 @@ console.log(app.obtenerCalificacion(8));
 console.log(app.obtenerCalificacion(9));
 console.log(app.obtenerCalificacion(10));
 console.log(app.obtenerCalificacion(11));
+
+console.log("Probando obtenerCercano100()");
+console.log(app.obtenerCercano100(98, 50, 105));
+console.log(app.obtenerCercano100(90, 70, 103));
+console.log(app.obtenerCercano100(98, 98, 98));
+
+console.log("Probando calcular()");
+console.log(app.calcular(7,8,'+'));
+console.log(app.calcular(7,8,'-'));
+console.log(app.calcular(7,8,'*'));
+console.log(app.calcular(7,8,'/'));
+console.log(app.calcular(7,8,'%'));
